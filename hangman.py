dceca148c6b193f767efc613d76ed71ec2955b4d
Stephen Wilson <slwilson10@gmail.com>
2015-07-29 13:39:37 -0400

    Cleanup
    Removed white space and some comments.

--- hangman.py
+++ hangman.py
@@ -1,12 +1,7 @@
-# 6.00 Problem Set 3
 # 
 # Hangman
 #
 
-
-# -----------------------------------
-# Helper code
-# (you don't need to understand this helper code)
 import random
 import string
 
@@ -16,37 +11,20 @@ wordlist = []
 def load_words():
     """
     Returns a list of valid words. Words are strings of lowercase letters.
-    
+
     Depending on the size of the word list, this function may
     take a while to finish.
     """
     print ("Loading word list from file...")
-    # inFile: file
     inFile = open(WORDLIST_FILENAME, 'r')
-    # line: string
     for w in inFile:
         word = (w.rstrip())
         wordlist.append(word)
-    # wordlist: list of strings
     print ("  ", len(wordlist), "words loaded.")
     return wordlist
 
 def choose_word(wordlist):
-    """
-    wordlist (list): list of words (strings)
-
-    Returns a word from wordlist at random
-    """
-    
     return random.choice(wordlist)
-    
-# end of helper code
-# -----------------------------------
-
-# actually load the dictionary of words and point to it with 
-# the wordlist variable so that it can be accessed from anywhere
-# in the program
-# your code begins here!
 
 def hide_word(randword, board, guesses):
   for letter in randword:
@@ -63,8 +41,7 @@ def swap_letters(randword, guesses, board):
     if letter in guesses:
       board.replace(letter)
   return 
-  
-  
+ 
 def get_guess(guesses):
   guess = str(input('Which letter do you guess? --> '))
   if len(guess) != 1:
@@ -79,7 +56,6 @@ def get_guess(guesses):
     print ('Your guesses are: %s ' % (guesses))
     return guess
 
-  
 def check_for_win(randword, guesses):
   right = 0
   for i in randword:
@@ -98,21 +74,16 @@ def main():
   guesses = []
   win = False
 
-
-  
   while True:
     print ('You have %d turns left.' % (max_tries))
     board = []
     hide_word(randword, board, guesses)
-    
     guess = str(get_guess(guesses))
     if guess not in randword:
       max_tries = max_tries - 1
     else: 
       pass
     win = check_for_win(randword, guesses)
-
-    
     if win == True:
       print ('You win!')
       break
@@ -122,8 +93,5 @@ def main():
   print ('The word was: ' + randword)
   print ('Game Over!')
 
-  
-
-# This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
   main()
